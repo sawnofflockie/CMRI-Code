985b35a0aabf037a7696a5cd1c9e7289c776de32
sawnofflockie <86487004+sawnofflockie@users.noreply.github.com>
2021-08-16 23:39:11 +0100

    Flickering lights and delayed start
    Now features flickering lights, delayed starts, so that one gas light comes on after another with a gap to simulate a little man walking from light to light, and a ramp up to full brightness again to simulate him turning on the gas tap.

--- CMRI_Nodes_Nano_1/CMRI_Nodes_Nano_1.ino
+++ CMRI_Nodes_Nano_1/CMRI_Nodes_Nano_1.ino
@@ -10,20 +10,22 @@
 #define CMRI_INPUTS 24
 #define CMRI_OUTPUTS 48
 
-//#define BAUD_RATE 19200
-#define BAUD_RATE 28800
+#define BAUD_RATE 19200
 
-#define NUMPWMOUTPUTS   2
+#define NUM_PWM_OUTPUTS   2
 
 #define PWM_NORMAL_LEVEL    3072 // Max level is 4096, chose 3072 so there is room to go up as well as down.
-#define PWM_VARIANCE        1024 // Max amount of variance up or down of LED level.
+#define PWM_VARIANCE        1023 // Max amount of variance up or down of LED level.
 
 // Servo frame rate must be 50Hz for analogue servos, can be up to 333Hz for digital servos, but in this case it is for LEDs.
 #define PWM_FRAME_RATE    120 // Can see 100Hz flicker, so made it a bit faster.
 
-// Both in milliseconds
+// All in milliseconds
 #define FLICKER_MIN_TIME    75
 #define FLICKER_MAX_TIME    350
+
+#define MIN_WAIT_PERIOD		20000
+#define WAIT_VARIANCE       10000
 
 // -----------------------------
 #define INPUT_RANGE_START    3
@@ -32,10 +34,40 @@
 #define OUTPUT_RANGE_END    13 // Pin 13 corresponds to the Arduino on-board LED
 // -----------------------------
 
+// -----------------------------
+// LED street light states
+// -----------------------------
+#define OFF         0
+#define ON          1
+#define WAIT_ON     2
+#define GOING_ON    3
+#define GOING_OFF   4
+#define WAIT_OFF	5
+
+#define LIGHT_LEVEL_STEP  100
+
+// -----------------------------
+// LED street light types
+// -----------------------------
+#define GAS         0
+#define ELECTRIC    1
+
+// Create structure to hold data about the lights
+struct light {
+    int lightNum;
+    int state;
+    int currentLevel;
+    int lightType; // Can be either GAS, ELECTRIC
+    unsigned long lastTime;
+    unsigned long flickerDelay;
+    unsigned long waitPeriod;
+	unsigned long waitStart;
+};
+
+light streetLight[NUM_PWM_OUTPUTS];
+
 bool requiredState = LOW;
 unsigned long currentTime;
-unsigned long lastTime[NUMPWMOUTPUTS];
-unsigned long flickerDelay[NUMPWMOUTPUTS];
 
 // Define the PCA9685 board addresses
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40); //setup the board address - defaults to 0x40 if not specified
@@ -46,31 +78,93 @@ Auto485 bus(DE_PIN); // Arduino pin 2 -> MAX485 DE and RE pins
 // Define CMRI connection with 24 inputs and 48 outputs
 CMRI cmri(CMRI_ADDR, CMRI_INPUTS, CMRI_OUTPUTS, bus);
 
-int outputLevel(int level) {
-    if (requiredState == HIGH) {
-        level = PWM_NORMAL_LEVEL - PWM_VARIANCE + random(PWM_VARIANCE * 2);
-    } else {
-        level = 0;
+int outputLevel(int level, int lightNum) {
+    switch (streetLight[lightNum].state) {
+		case OFF:
+		case WAIT_ON:
+			streetLight[lightNum].currentLevel = OFF;
+		break;
+        case GOING_ON:
+            if (streetLight[lightNum].currentLevel < PWM_NORMAL_LEVEL) {
+                streetLight[lightNum].currentLevel += LIGHT_LEVEL_STEP;
+            } else {
+                streetLight[lightNum].state = ON;
+            }
+        break;
+        case ON:
+		case WAIT_OFF:
+			if (streetLight[lightNum].lightType == GAS) {
+				streetLight[lightNum].currentLevel = PWM_NORMAL_LEVEL - PWM_VARIANCE + random(PWM_VARIANCE * 2);
+			} else {
+				streetLight[lightNum].currentLevel = PWM_NORMAL_LEVEL;
+			}
+        break;
+        case GOING_OFF:
+            if (streetLight[lightNum].currentLevel > OFF) {
+                streetLight[lightNum].currentLevel -= LIGHT_LEVEL_STEP;
+            } else {
+                streetLight[lightNum].state = OFF;
+            }
+        break;
     }
-    return level;
+    return streetLight[lightNum].currentLevel;
 }
 
-void lightLED() {
+void lightLED(int pwmOutput) {
     int level = 0;
-    currentTime = millis();
-    for (int pwmOutput = 0; pwmOutput < NUMPWMOUTPUTS; pwmOutput++) {
-        if ((currentTime - lastTime[pwmOutput]) > flickerDelay[pwmOutput]) {
-            level = outputLevel(level);
-            pwm.writeMicroseconds(pwmOutput, level);
-            flickerDelay[pwmOutput] = random(FLICKER_MIN_TIME, FLICKER_MAX_TIME);
-            lastTime[pwmOutput] = currentTime;
-        }
-    }
+	if ((currentTime - streetLight[pwmOutput].lastTime) > streetLight[pwmOutput].flickerDelay) {
+		level = outputLevel(level, pwmOutput);
+		pwm.writeMicroseconds(pwmOutput, level);
+		streetLight[pwmOutput].flickerDelay = random(FLICKER_MIN_TIME, FLICKER_MAX_TIME);
+		streetLight[pwmOutput].lastTime = currentTime;
+	}
+}
+
+void setup_wait_period(int pwmOutput, unsigned long currentTime) {
+	streetLight[pwmOutput].waitPeriod = (MIN_WAIT_PERIOD * (unsigned long)pwmOutput) + random(WAIT_VARIANCE);
+	streetLight[pwmOutput].waitStart = currentTime;
 }
 
 void process_outputs() {
+    currentTime = millis();
     requiredState = cmri.get_bit(0); //Bit 0 = address 2001 in JMRI, LED output 1
-    lightLED();
+    for (int pwmOutput = 0; pwmOutput < NUM_PWM_OUTPUTS; pwmOutput++) {
+		switch (streetLight[pwmOutput].state) {
+			case OFF:
+				if (requiredState == HIGH) {
+					if (pwmOutput != 0) {
+						streetLight[pwmOutput].state = WAIT_ON;
+						setup_wait_period(pwmOutput, currentTime);
+					} else {
+						streetLight[pwmOutput].state = GOING_ON;
+					}
+				}
+			break;
+			case WAIT_ON:
+				if (currentTime > streetLight[pwmOutput].waitStart + streetLight[pwmOutput].waitPeriod) {
+					streetLight[pwmOutput].state = GOING_ON;
+				}
+			break;
+			case ON:
+				if (requiredState == LOW) {
+					if (pwmOutput != 0) {
+						streetLight[pwmOutput].state = WAIT_OFF;
+						setup_wait_period(pwmOutput, currentTime);
+					} else {
+						streetLight[pwmOutput].state = GOING_OFF;
+					}
+				}
+			break;
+			case WAIT_OFF:
+				if (currentTime > streetLight[pwmOutput].waitStart + streetLight[pwmOutput].waitPeriod) {
+					streetLight[pwmOutput].state = GOING_OFF;
+				}
+			case GOING_ON:
+			case GOING_OFF:
+			break;
+		}
+		lightLED(pwmOutput);
+    }
 }
 
 void setup() {
@@ -87,9 +181,15 @@ void setup() {
 
     randomSeed(analogRead(0));
 
-    for (int pwmOutput = 0; pwmOutput < NUMPWMOUTPUTS; pwmOutput++) {
-        flickerDelay[pwmOutput] = random(FLICKER_MIN_TIME, FLICKER_MAX_TIME);
-        lastTime[pwmOutput] = 0;
+    for (int pwmOutput = 0; pwmOutput < NUM_PWM_OUTPUTS; pwmOutput++) {
+        streetLight[pwmOutput].lightNum = pwmOutput;
+        streetLight[pwmOutput].state = OFF;
+        streetLight[pwmOutput].currentLevel = 0;
+        streetLight[pwmOutput].lightType = GAS;
+        streetLight[pwmOutput].lastTime = 0;
+        streetLight[pwmOutput].flickerDelay = random(FLICKER_MIN_TIME, FLICKER_MAX_TIME);
+        streetLight[pwmOutput].waitPeriod = 0;
+        streetLight[pwmOutput].waitStart = 0;
     }
     // Start the serial connection
     Serial.begin(BAUD_RATE); //Baud rate of 19200, ensure this matches the baud rate in JMRI, using a faster rate can make processing faster but can also result in incomplete data
@@ -98,8 +198,6 @@ void setup() {
     // Initialize PCA9685 board
     pwm.begin();
     pwm.setPWMFreq(PWM_FRAME_RATE);  // This is the maximum PWM frequency
-    pwm.writeMicroseconds(0, PWM_NORMAL_LEVEL);
-
 }
 
 void loop(){
@@ -109,7 +207,7 @@ void loop(){
     // Only include lines that are required. This reduces processing time - delete or comment out lines that are not required
 
     // Do not read 0, 1 or 2
-    cmri.set_bit(0, !digitalRead(3));  //Bit 0 = address 2001 in JMRI, Light sensor 1
+    // cmri.set_bit(0, !digitalRead(3));  //Bit 0 = address 2001 in JMRI, Light sensor 1
     // cmri.set_bit(1, !digitalRead(4));  //Bit 1 = address 2002 in JMRI, Light sensor 2
 
     // PROCESS OUTPUTS

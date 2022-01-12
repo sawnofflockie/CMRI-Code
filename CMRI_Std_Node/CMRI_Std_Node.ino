92b6a42745f70ee7ff42970db32e551bf55d9c65
sawnofflockie <86487004+sawnofflockie@users.noreply.github.com>
2022-01-12 23:08:42 +0000

    Update CMRI_Std_Node.ino
    Standardised sensor node code.

--- CMRI_Std_Node/CMRI_Std_Node.ino
+++ CMRI_Std_Node/CMRI_Std_Node.ino
@@ -1,3 +1,7 @@
+// ========================================
+// Standard CMRI sensor node (Arduino Nano)
+// ========================================
+
 // Include libraries
 #include <CMRI.h>
 #include <Auto485.h>
@@ -13,26 +17,37 @@
 #define SERIAL_BAUD_RATE 19200
 
 // -----------------------------
-#define input_range_start 3
-#define input_range_end 7
-#define output_range_start 8
-#define output_range_end 13 // Pin 13 corresponds to the Arduino on-board LED
+#define INPUT_RANGE_START 3
+#define INPUT_RANGE_END 12
 // -----------------------------
 
 // -----------------------------
-// LED states
+// States
 // -----------------------------
-#define OFF         0 // Light switched off.
-#define ON          1 // Light switched on.
+#define OFF         0
+#define ON          1
 
 // -----------------------------
 // Global variables
 // -----------------------------
-bool IR_sensor = OFF;
-bool light_sensor_1 = OFF;
-bool light_sensor_2 = OFF;
-bool arduino_LED = OFF;
-bool arduino_last_state = OFF;
+bool sensor_1 = OFF;
+bool sensor_2 = OFF;
+bool sensor_3 = OFF;
+bool sensor_4 = OFF;
+bool sensor_5 = OFF;
+bool sensor_6 = OFF;
+bool sensor_7 = OFF;
+bool sensor_8 = OFF;
+bool sensor_9 = OFF;
+bool sensor_10 = OFF;
+
+// -----------------------------
+// Function prototypes
+// -----------------------------
+void setup(void);
+void loop(void);
+void readDigitalSensors(void);
+void sendToCMRI(void);
 
 // Setup serial communication
 Auto485 bus(DE_PIN); // Arduino pin 2 -> MAX485 DE and RE pins
@@ -40,51 +55,49 @@ Auto485 bus(DE_PIN); // Arduino pin 2 -> MAX485 DE and RE pins
 // Define CMRI connection with 64 inputs and 128 outputs
 CMRI cmri(CMRI_ADDR, CMRI_INPUTS, CMRI_OUTPUTS, bus);
 
-void setup() {
+void setup(void) {
 
     // SET PINS TO INPUT OR OUTPUT
 
-    for (int i=input_range_start; i<=input_range_end; i++) {
-           pinMode(i, INPUT_PULLUP);       // define sensor shield pins 3 to 7 as inputs - 5 inputs.
-    }
-
-    for (int i=output_range_start; i<=output_range_end; i++) {
-           pinMode(i, OUTPUT);      // define sensor shield pins 8 to 13 as outputs - 5 outputs, plus pin 13 which is the built-in LED.
+    for (int i=INPUT_RANGE_START; i<=INPUT_RANGE_END; i++) {
+           pinMode(i, INPUT_PULLUP);       // define sensor shield pins 3 to 12 as inputs - 10 inputs.
     }
 
     // Start the serial connection
-    Serial.begin(SERIAL_BAUD_RATE); //Baud rate of 19200, ensure this matches the baud rate in JMRI, using a faster rate can make processing faster but can also result in incomplete data
-    bus.begin(BAUD_RATE);
+    Serial.begin(SERIAL_BAUD_RATE);     // Used for debug output.
+    // bus.begin(BAUD_RATE, SERIAL_8N2); // May need this version (8 bit, no parity, 2 stop bits) if JMRI doesn't understand the standard single stop bit.
+    bus.begin(BAUD_RATE);               // Ensure this matches the baud rate in JMRI.
 }
 
-void loop(){
+void loop(void){
     cmri.process();
-    readSensors();
+    readDigitalSensors();
     sendToCMRI();
-    readFromCMRI();
-    processOutputs();
 }
 
-void readSensors(void) {
+void readDigitalSensors(void) {
     // Do not read 0, 1 or 2
-    IR_sensor = digitalRead(3);         // Pin 3 on Arduino
-    light_sensor_1 = digitalRead(4);    // Pin 4 on Arduino
-    light_sensor_2 = digitalRead(5);    // Pin 5 on Arduino
+    sensor_1 = digitalRead(3);      // Pin 3 on Arduino
+    sensor_2 = digitalRead(4);      // Pin 4 on Arduino
+    sensro_3 = digitalRead(5);      // Pin 5 on Arduino
+    sensor_4 = digitalRead(6);      // Pin 6 on Arduino
+    sensor_5 = digitalRead(7);      // Pin 7 on Arduino
+    sensor_6 = digitalRead(8);      // Pin 8 on Arduino
+    sensor_7 = digitalRead(9);      // Pin 9 on Arduino
+    sensor_8 = digitalRead(10);     // Pin 10 on Arduino
+    sensor_9 = digitalRead(11);     // Pin 11 on Arduino
+    sensor_10 = digitalRead(12);    // Pin 12 on Arduino
 }
 
 void sendToCMRI(void) {
-    cmri.set_bit(0, !IR_sensor);        //Bit 0 = address 1001 in JMRI, IR sensor 1
-    cmri.set_bit(1, !light_sensor_1);   //Bit 1 = address 1002 in JMRI, Light Level Sensor 1
-    cmri.set_bit(2, !light_sensor_2);   //Bit 2 = address 1003 in JMRI, Home made Light Level Sensor 1
-}
-
-void readFromCMRI(void) {
-    arduino_LED = cmri.get_bit(0);      // Bit 0 = address 1001 in JRMI, LED output 1
-}
-
-void processOutputs(void) {
-    if (arduino_last_state != arduino_LED) {
-        arduino_last_state = arduino_LED;
-        digitalWrite(13, arduino_LED);  // Pin 13 corresponds to the Arduino on-board LED
-    }
+    cmri.set_bit(0, !sensor_1);     //Bit 0 = address 1001 in JMRI/CMRI
+    cmri.set_bit(1, !sensor_2);     //Bit 1 = address 1002 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_3);     //Bit 2 = address 1003 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_4);     //Bit 2 = address 1004 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_5);     //Bit 2 = address 1005 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_6);     //Bit 2 = address 1006 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_7);     //Bit 2 = address 1007 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_8);     //Bit 2 = address 1008 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_9);     //Bit 2 = address 1009 in JMRI/CMRI
+    cmri.set_bit(2, !sensor_10);    //Bit 2 = address 1010 in JMRI/CMRI
 }
